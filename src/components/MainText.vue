6ba2aa715dc37c41ba1bb18aa6f42d20852f6056
PiotrO9 <piotr.ostrowski02@wp.pl>
2022-12-22 10:59:45 +0100

    Finished typewriter animation

--- src/components/MainText.vue
+++ src/components/MainText.vue
@@ -9,8 +9,28 @@
 
 <script>
 export default {
+    mounted() {
+        const TypeWriter = document.getElementById("TypeWriter");
 
+        const textLoad = () => {
+            setTimeout(() => {
+                TypeWriter.textContent = "Testing"
+                document.documentElement.style.setProperty('--steps', "7")
+            }, 0)
+            setTimeout(() => {
+                TypeWriter.textContent = "Buissnes"
+                document.documentElement.style.setProperty('--steps', "8")
+            }, 5000)
+            setTimeout(() => {
+                TypeWriter.textContent = "Fun"
+                document.documentElement.style.setProperty('--steps', "3")
+            }, 10000)
+            setTimeout(textLoad, 15000)
+        }
+            textLoad()
+    }
 }
+
 </script>
 
 <style lang="scss">
