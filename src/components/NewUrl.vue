f72339cc30cfb37b59885cc545cf429a46f412b0
PiotrO9 <piotr.ostrowski02@wp.pl>
2022-12-28 12:46:04 +0100

    Add more animations for new url component

--- src/components/NewUrl.vue
+++ src/components/NewUrl.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="NewUrl">
-    <span>Test text longer than usually</span>
+    <span id="TextToCopy">Test text longer than usually</span>
     <div id="CopyIcon">
-        <fa id="clipboard" icon="clipboard"/>
-        <fa @click="CloseNewUrl" id="close" icon="close"/>
-        <div v-if="copiedVisibility" id="CopiedAlert">
-            <span>Copied</span>
-        </div>
+        <fa @click="CopyTextToClipboard" id="clipboard" icon="clipboard"/>
+        <transition name="close">
+            <fa @click="CloseNewUrl" v-if="closeVisibility" id="close" icon="close"/>
+        </transition>
+            <div v-if="copiedVisibility" id="CopiedAlert">
+                <span>Copied</span>
+            </div>
     </div>
   </div>
 </template>
@@ -15,12 +17,21 @@
 export default {
     data() {
         return{
-            copiedVisibility: false
+            copiedVisibility: false,
+            closeVisibility: true
         }
     },
     methods: {
         CloseNewUrl() {
             this.$emit("close")
+            this.closeVisibility = false
+        },
+        CopyTextToClipboard() {
+            const TextToCopy = document.getElementById("TextToCopy")
+
+            navigator.clipboard.writeText(TextToCopy.innerText)
+            this.copiedVisibility = true
+            setTimeout(() => { this.copiedVisibility = false }, 2000)
         }
     }
 }
@@ -28,4 +39,26 @@ export default {
 
 <style lang="scss">
 @import "../Styles/NewUrlStyles.scss";
+
+.new-url-leave-active {
+
+    #CopyIcon {
+
+        #close {
+
+            animation: closeDisappear 1s forwards;
+        }
+    }
+}
+
+@keyframes closeDisappear {
+    0% {
+        scale: 100%;
+        transform: rotate(0);
+    }
+    100% {
+        scale: 0%;
+        transform: rotate(360);
+    }
+}
 </style>
