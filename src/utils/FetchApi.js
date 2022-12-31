a0a405b62c40b7c73403f200d4b3191a5ed92c65
PiotrO9 <piotr.ostrowski02@wp.pl>
2022-12-31 15:40:12 +0100

    Add .env file with apikey

--- src/utils/FetchApi.js
+++ src/utils/FetchApi.js
@@ -4,7 +4,7 @@ async function FetchApi(LinkToShorten) {
     return await fetch('https://api-ssl.bitly.com/v4/shorten', {
         method: 'POST',
         headers: {
-            'Authorization': `Bearer ${UrlShortenerApiKey}`,
+            'Authorization': `Bearer ${process.env.VUE_APP_NETLIFY_API_KEY}`,
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ "long_url": `${LinkToShorten}`, "domain": "bit.ly" })
